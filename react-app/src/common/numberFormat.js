828eb2b6a43011c64686b2c8c04e84c4c59c1ad6
Unknown <locdtptit@gmail.com>
2020-07-09 23:17:11 +0700

    update server

--- react-app/src/common/numberFormat.js
+++
@@ -1,3 +0,0 @@
-export const numberFormat = (value) => {
-  return value.toLocaleString('it-IT', {currency : 'VND'});
-};
--- react-app/src/components/loading/loading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import sneakers from './sneakers.svg';
-import './styles.scss';
-
-export default function Loading() {
-    return (
-        <img id="shoes" src={sneakers}></img>
-    );
-}
